e53081a0feaea2c802dcb28aa482cbeb5eb4a52e
Ruby <robabenyoussef@gmail.com>
2025-09-02 10:55:12 +0100

    new

---
+++ .angular/cache/20.1.0/ancfcc-app/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-IZAOII4Y.js";
+import "./chunk-W3S2ADXR.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
